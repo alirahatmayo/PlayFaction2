cc9e1081afbec2337dd8d36c72a6a1b7dad943d7
alirahatmayo <ali.rahat.mayo@gmail.com>
2019-02-08 01:00:07 +0500

    UserProfile
    Added API for user profile where if user is authenticated, he can edit
    his info

--- settings.py
+++ settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #Installed packages
+    'rest_framework',
+    'phonenumber_field',
+
+    #local apps
+    'userprofile'
 ]
 
 MIDDLEWARE = [
